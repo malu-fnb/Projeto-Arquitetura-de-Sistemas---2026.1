4ecf3af102d26ce412109a936738c21af7a6571b
DavidCands <davidcandido1914@gmail.com>
2026-05-25 01:14:11 -0300

    Criando um verificador para os Handlers

--- iot-middleware/middleware/src/handlers/temperatura.py
+++ iot-middleware/middleware/src/handlers/temperatura.py
@@ -1,16 +1,31 @@
-def processar_temperatura(payload):
-    temperatura = payload.get("valor")
+import logging
 
-    if temperatura is None:
-        raise ValueError("Temperatura ausente")
+from handlers.verificador_handler import validar_valor, criar_resposta
+
+LOGGER = logging.getLogger(__name__)
+
+def processar_temperatura(payload, correlation_id):
+    valor = payload.get("valor")
+
+    validar_valor(valor)
 
     status = "normal"
 
-    if temperatura > 40:
+    if valor > 40:
         status = "critico"
 
-    return{
-        "sensor": "temperatura",
-        "valor": temperatura,
-        "status": status
-    }
+    resultado = criar_resposta(
+        sensor = "temperatura",
+        valor = valor,
+        status = status,
+        correlation_id = correlation_id,
+    )
+
+    LOGGER.info(
+        {
+            "evento": "temperatura_processada",
+            "resultado": resultado,
+        }
+    )
+
+    return resultado
